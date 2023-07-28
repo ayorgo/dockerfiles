f6c702a5ccf4ac18a4a9232684e0fd6046a4daa9
ayorgo <george.adzhygai@gmail.com>
2023-07-28 11:58:54 +0200

    Reduce ESC delay to zero when swtitching vi modes in ipython

--- 3.11/ipython_config.py
+++ 3.11/ipython_config.py
@@ -1,1 +1,5 @@
 c.TerminalInteractiveShell.editing_mode = 'vi'
+
+# Reduce ESC delay to zero when switching vim modes
+# https://github.com/ipython/ipython/issues/13443
+c.TerminalInteractiveShell.emacs_bindings_in_vi_insert_mode = False
