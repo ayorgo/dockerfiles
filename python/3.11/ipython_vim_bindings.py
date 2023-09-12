8cefdcfcdaefe2d802f95bca6fc8b1dd46c37cfd
ayorgo <george.adzhygai@gmail.com>
2023-09-12 22:20:47 +0200

    Fix python vim bindings for python:3.11

--- python/3.11/ipython_vim_bindings.py
+++ python/3.11/ipython_vim_bindings.py
@@ -6,8 +6,11 @@ from IPython import get_ipython
 from prompt_toolkit.enums import DEFAULT_BUFFER
 from prompt_toolkit.filters import HasFocus, ViNavigationMode
 from prompt_toolkit.key_binding.bindings.named_commands import get_by_name
+from prompt_toolkit.clipboard.pyperclip import PyperclipClipboard
 
 ip = get_ipython()
+# if getattr(ip, "pt_app", None):
+#     ip.pt_app.clipboard = PyperclipClipboard()
 registry = ip.pt_app.key_bindings
 ph = get_by_name('previous-history')
 nh = get_by_name('next-history')
